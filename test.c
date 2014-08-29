0a688228bf2f52b58270a06b95edf992e4216209
allen lee <936938813@qq.com>
2014-08-29 09:14:36 +0800

    A test program

---
+++ test.c
@@ -0,0 +1,13 @@
+//========================================
+//Name        :test.c
+//Author      :allen lee
+//Version     :
+//Copyright   : 
+//Description :this is a test c program
+//========================================
+#include <stdio.h>
+int main(){
+    printf("Hello, this is my first program on github!\n");
+    return 0;
+
+}
